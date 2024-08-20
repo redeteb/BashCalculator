570a986558f427ae464e2170211fa6fab2c38c94
Redete <bezabehredete1@gmail.com>
2024-08-20 17:14:37 +0000

    Adding Bash Calculator

---
+++ bashcalculator.sh
@@ -0,0 +1,23 @@
+#!/bin/bash/
+
+#Prompt user to enter two integers
+#Prompt user to choose a operation
+#Return calculated output
+#Make sure appropriate error messages are addressed
+
+echo "Please enter your first number."
+
+read  
+
+echo "Please enter your secind number."
+
+read 
+
+echo "Please choose an operator: addition, subratction, multiplication, subtraction."
+
+read
+
+echo "Thank you! Your answer is:,,"
+
+
+
