4b980f92b64b591e7a61e7670ca8210eecf140be
Redete <bezabehredete1@gmail.com>
2024-08-20 18:39:48 +0000

    v3 of calculator

--- bashcalculator.sh
+++ bashcalculator.sh
@@ -1,31 +1,6 @@
-##########################################
 
-#!/bin/bash/
+#!/bin/bash
 
-#Prompt user to enter two integers
-#Prompt user to choose a operation
-#Return calculated output
-#Make sure appropriate error messages are addressed
-
-echo "Please enter your first number."
-
-read  
-
-echo "Please enter your secind number."
-
-read 
-
-echo "Please choose an operator: addition, subratction, multiplication, subtraction."
-
-read
-
-echo "Thank you! Your answer is:,,"
-#################################
-
-
-
-
-# Main program starts here
 echo "Welcome to Bash Calculator!"
 
 # Input the first integer
